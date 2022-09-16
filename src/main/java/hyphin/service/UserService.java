c7b39950f872f3fe5c87676875f4b8e8feb59732
Dmytro Ostapchenko <dmytro.ostapchenko@aspira.global>
2022-09-16 16:08:44 +0100

    set acceptable both encrypted and non-encrypted passwords

--- src/main/java/hyphin/service/UserService.java
+++ src/main/java/hyphin/service/UserService.java
@@ -3,6 +3,8 @@ package hyphin.service;
 import hyphin.model.User;
 import hyphin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +14,13 @@ public class UserService{
     @Autowired
     private UserRepository userRepository;
 
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(8);
+
     public User findByUserNameAndPassword(String userName, String password) {
         List<User> users = (List<User>) userRepository.findAll();
         for (User user : users) {
-            if(user != null && user.getEmail() != null && user.getEmail().equalsIgnoreCase(userName) && user.getPassword() != null && user.getPassword().equals(password))
+            if(user != null && user.getEmail() != null && user.getEmail().equalsIgnoreCase(userName) && user.getPassword() != null &&
+                    (user.getPassword().equals(password) || user.getPassword().equals(passwordEncoder.encode(password))))
                 return user;
         }
         return null;
